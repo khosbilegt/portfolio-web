99b4805c164d2e37596cef102d56e944f0ef8338
Khosbilegt Bilegsaikhan <khosookoo@gmail.com>
2025-02-24 22:57:01 +0800

    added hash param to scroll in home page

--- src/features/home/components/HomeCard.tsx
+++ src/features/home/components/HomeCard.tsx
@@ -1,13 +1,11 @@
 import { Stack, Title } from "@mantine/core";
 import { motion } from "motion/react";
+import { forwardRef } from "react";
 
-function HomeCard({
-  title,
-  children,
-}: {
-  title: string;
-  children: React.ReactNode;
-}) {
+const HomeCard = forwardRef<
+  HTMLDivElement,
+  { title: string; children: React.ReactNode }
+>(({ title, children }, ref) => {
   return (
     <motion.div
       initial={{ opacity: 0.0, y: 40 }}
@@ -15,12 +13,12 @@ function HomeCard({
       transition={{ duration: 0.8, delay: 0.2, ease: "easeInOut" }}
       viewport={{ once: true }}
     >
-      <Stack gap={"md"} align="center">
+      <Stack gap={"md"} align="center" ref={ref}>
         <Title>{title}</Title>
         {children}
       </Stack>
     </motion.div>
   );
-}
+});
 
 export default HomeCard;
--- src/features/home/pages/Home.tsx
+++ src/features/home/pages/Home.tsx
@@ -4,16 +4,59 @@ import Experience from "../components/Experience";
 import HomeCard from "../components/HomeCard";
 import Education from "../components/Education";
 import Certificates from "../components/Certificates";
+import { useScrollIntoView } from "@mantine/hooks";
+import { useLocation } from "react-router";
+import { useEffect } from "react";
 
 function Home() {
+  const { hash } = useLocation();
+
+  const { scrollIntoView: scrollToExperience, targetRef: experienceRef } =
+    useScrollIntoView<HTMLDivElement>({
+      offset: 60,
+    });
+  const { scrollIntoView: scrollToEducation, targetRef: educationRef } =
+    useScrollIntoView<HTMLDivElement>({
+      offset: 60,
+    });
+  const { scrollIntoView: scrollToCertificates, targetRef: certificatesRef } =
+    useScrollIntoView<HTMLDivElement>({
+      offset: 60,
+    });
+
+  useEffect(() => {
+    console.log(hash);
+    switch (hash) {
+      case "#experience":
+        scrollToExperience();
+        break;
+      case "#education":
+        scrollToEducation();
+        break;
+      case "#certificates":
+        scrollToCertificates();
+        break;
+      default:
+        break;
+    }
+  }, [hash]);
+
   return (
     <Stack gap={"lg"}>
       <Hero />
-      <HomeCard title="Experience" children={<Experience />} />
+      <HomeCard
+        title="Experience"
+        children={<Experience />}
+        ref={experienceRef}
+      />
       <Flex h={100} />
-      <HomeCard title="Education" children={<Education />} />
+      <HomeCard title="Education" children={<Education />} ref={educationRef} />
       <Flex h={100} />
-      <HomeCard title="Certificates" children={<Certificates />} />
+      <HomeCard
+        title="Certificates"
+        children={<Certificates />}
+        ref={certificatesRef}
+      />
       <Flex h={100} />
     </Stack>
   );
