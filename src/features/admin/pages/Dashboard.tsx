b56dd2a09af9cf09b67315e1fe8c66b149a1f4a0
Khosbilegt Bilegsaikhan <khosookoo@gmail.com>
2025-03-02 01:09:18 +0800

    added grafana dashboard

--- src/features/admin/pages/Dashboard.tsx
+++ src/features/admin/pages/Dashboard.tsx
@@ -1,5 +1,22 @@
+import { Anchor, Stack, Text } from "@mantine/core";
+
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <Stack mih={"80vh"}>
+      <Text>
+        The Dashboard is delegated to Grafana, self-hosted at{" "}
+        <Anchor href="https://grafana.khosbilegt.dev">
+          grafana.khosbilegt.dev
+        </Anchor>
+      </Text>
+      <iframe
+        style={{
+          height: "80vh",
+        }}
+        src="https://grafana.khosbilegt.dev/d/b6a8de2e-053d-488a-8533-9a15364c5b6a/jvm-micrometer?orgId=1&refresh=30s&from=1740761263941&to=1740847663941"
+      />
+    </Stack>
+  );
 }
 
 export default Dashboard;
