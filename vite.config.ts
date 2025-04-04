0fc052bbf782f9b4f1678277d9687745ec5956dc
Khosbilegt Bilegsaikhan <khosookoo@gmail.com>
2025-04-04 21:32:11 +0800

    started nextjs migration

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import { compression } from 'vite-plugin-compression2'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), compression()],
-})
